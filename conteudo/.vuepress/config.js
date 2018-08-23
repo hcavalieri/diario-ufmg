2364fda08ff7e2506d6ea7be46e95c7811ed0a3b
hcavalieri <hcavalieridoro@gmail.com>
2018-08-23 11:02:07 -0300

    PWA e """logo"""

--- conteudo/.vuepress/config.js
+++ conteudo/.vuepress/config.js
@@ -19,6 +19,8 @@ const monografiaSidebar = {
   ]
 };
 
+const themeColor= '#42b983';
+
 module.exports = {
   title: "Diário UFMG",
   description: "Anotações, resenhas e aprendizados sobre o que vejo no curso de economia na UFMG",
@@ -26,12 +28,31 @@ module.exports = {
   markdown: {
     lineNumbers: true,
   },
+  head: [
+    ['link', { rel: 'icon', href: '/icons/icon-128x128.png' }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['link', { rel: 'canonical', href: 'https://ufmg.henrique.codes' }],
+    ['meta', { name: 'theme-color', content: themeColor }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: `/icons/icon-152x152.png` }],
+    // ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/icons/icons-144x144.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: themeColor }]
+  ],
+  serviceWorker: true,
   themeConfig: {
     repo: 'hcavalieri/diario-ufmg',
     repoLabel: 'Contribua :)',
     docsDir: 'conteudo/docs',
     editLinks: true,
     editLinkText: 'Edite essa página pelo GitHub',
+    serviceWorker: {
+      updatePopup: {
+        message: "Tem novo conteúdo disponível.",
+        buttonText: "Atualizar!",
+      },
+    },
     nav: [
       { text: 'Inovação industrial', link: '/economia-inovacao-industrial/' },
       { text: 'Monografia', link: '/monografia/' },
